53b07886d2f600437225adedfc74bfabe53f157e
ivanglpz <miltonivan117@gmail.com>
2026-01-08 21:01:13 -0600

    public login handler controller auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,26 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-
+import { Public } from './roles.decorator';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInDto: { email: string; password: string }) {
     return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+  @Get('profile')
+  getProfile(@Request() req: { user: { email: string } }) {
+    return req.user;
   }
 }
