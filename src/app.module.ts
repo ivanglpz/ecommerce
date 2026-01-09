9bde192c12e9383e0dc328276bf871340a98f693
ivanglpz <miltonivan117@gmail.com>
2026-01-08 20:50:19 -0600

    app module provider auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { AuthGuard } from './auth/auth.guard';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 @Module({
   imports: [UserModule, PrismaModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: 'APP_GUARD',
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
